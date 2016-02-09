4daf49a6ac3f5f93424830830934a4febc59ef20
Alex Smith <alex.smith@redhat.com>
2016-02-09 18:33:55 -0500

    Fix syntax error.

--- manifests/docker/images.pp
+++ manifests/docker/images.pp
@@ -5,7 +5,7 @@
 class graphite::docker::images (
   $build_dir           = '/opt/docker-graphite',
   $vcs_revision        = '1.1',
-  $db_host             = 'graphite-db'
+  $db_host             = 'graphite-db',
   $db_user             = 'graphite',
   $db_password         = '',
   $django_secret_key   = 'UNSAFE_SECRET',
